9f8a99e4e58d9a1a9cb87711e4d8e28da0a585de
Vaishal Shah <75125557+Vaishalshah2611@users.noreply.github.com>
2025-08-07 14:42:02 -0400

    Add generated_modules/aws_db_instance_aws_db_subnet_group_aws_security_group_0b33b5c0_1754577873/variables.tf

---
+++ generated_modules/aws_db_instance_aws_db_subnet_group_aws_security_group_0b33b5c0_1754577873/variables.tf
@@ -0,0 +1,22 @@
+variable "prefix" {
+  description = "Prefix to use for resource names"
+  type        = string
+  default     = "myapp"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "db_username" {
+  description = "Username for the database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the database"
+  type        = string
+  sensitive   = true
+}
